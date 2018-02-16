615f8cdb00d766066a5232ed0d7aa5dd2db34e45
huongung155 <huongung155@gmail.com>
2018-02-16 13:30:20 +0700

    test

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -7,13 +7,14 @@ $('#campground-search').on('input', function() {
         $('#campground-grid').html('');
         data.forEach(function(campground) {
             $('#campground-grid').append(`
-                <div class="col-md-3 col-sm-6">
-                    <div class="thumbnail">
-                        <img src="${ campground.image }">
-                        <div class="caption">
-                            <h4>${ campground.name }</h4>
+                <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="${ campground.image }" alt="${ campground.name }">
+                        <div class="card-body">
+                            <h4 class="card-title">${ campground.name }</h4>
+                            <p class="card-text">${campground.description.length > 50 ? campground.description.substring(0, 50) + "...": campground.description}</p>
+                            <a href="/campgrounds/${campground._id}" class="btn btn-primary">More Info</a>
                         </div>
-                        <p><a href="/campgrounds/${ campground._id }" class="btn btn-primary">More Info</a></p>
                     </div>
                 </div>
             `);
